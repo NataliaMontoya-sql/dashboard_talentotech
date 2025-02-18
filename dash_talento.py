62037f5262a85bb4632cb694a1b4fcad84abfc72
Natalia Montoya <nataliajaramillo1139598@correo.itm.edu.co>
2025-02-18 12:49:46 -0500

    Update dash_talento.py

--- dash_talento.py
+++ dash_talento.py
@@ -56,7 +56,7 @@ const DashboardEducativo = () => {
                     strokeWidth="3"
                   />
                   <path
-                    d="M18 2.0845
+                    d="M18" 2.0845
                       a 15.9155 15.9155 0 0 1 0 31.831
                       a 15.9155 15.9155 0 0 1 0 -31.831"
                     fill="none"
