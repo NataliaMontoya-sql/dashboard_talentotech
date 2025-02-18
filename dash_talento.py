97c6dd0e7d1297147a153f28ba2ebaa453c5b74a
Natalia Montoya <nataliajaramillo1139598@correo.itm.edu.co>
2025-02-18 09:03:08 -0500

    Update dash_talento.py

--- dash_talento.py
+++ dash_talento.py
@@ -11,7 +11,7 @@ import time
 st.set_page_config(
     page_title="Dashboard Educativo Avanzado",
     page_icon="🎓",
-    layout="ultrawide",
+    layout="wide",
     initial_sidebar_state="expanded"
 )
 
